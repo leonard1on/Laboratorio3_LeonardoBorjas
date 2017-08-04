ad48a1ca1261425f181e2cba66cf4d1a53b1df9f
Leonardo <leonardborj@gmail.com>
2017-08-04 15:47:28 -0600

    Barcos tienen vida

--- Laboratorio3_LeonardoBorjas.cpp
+++ Laboratorio3_LeonardoBorjas.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
+#include <string>
 char** crearmatriz(int);
 void imprimirmatriz(char**, int, int, int);
 char** llenarmatriz1(char**, int);
 char** llenarmatriz2(char**, int);
+string random();
 using namespace std;
 int main(){
 	int size=8;
@@ -12,9 +15,18 @@ int main(){
 	int coordy=0;
 	int shot1=0;
 	int shot2=0;
+	int barco11=3;
+	int barco12=3;
+	int barco13=3;
+	int barco14=3;
+	int barco21=3;
+	int barco22=3;
+	int barco23=3;
+	int barco24=3;
 	char** matriz1=crearmatriz(size);
 	char** matriz2=crearmatriz(size);
-	
+	srand(time(NULL));
+		
 	do{
 		if(salir==0){
 			matriz1=llenarmatriz1(matriz1,size);	
@@ -30,6 +42,44 @@ int main(){
 				cout<<"Le haz dado a un barco";
 				matriz2[coordx][coordy];
 				shot2+=1;
+				
+				if(coordx==1 && coordy==1)
+					barco11-=1;
+				if(coordx==1 && coordy==2)
+					barco11-=1;
+				if(coordx==1 && coordy==3)
+					barco11-=1;		
+
+				if(coordx==2 && coordy==7)
+					barco12-=1;
+				if(coordx==3 && coordy==7)
+					barco12-=1;
+				if(coordx==4 && coordy==7)
+					barco12-=1;
+
+				if(coordx==3 && coordy==4)
+					barco13-=1;
+				if(coordx==4 && coordy==4)
+					barco13-=1;
+				if(coordx==5 && coordy==4)
+					barco13-=1;
+
+				if(coordx==7 && coordy==1)
+					barco14-=1;
+				if(coordx==7 && coordy==2)
+					barco14-=1;
+				if(coordx==7 && coordy==3)
+					barco14-=1;
+
+				
+				if(barco11==0)
+					cout<<"Haz hundido el barco 1 del oponente!"<<endl;
+				if(barco12==0)
+					cout<<"Haz hundido el barco 2 del oponente!"<<endl;
+				if(barco13==0)
+					cout<<"Haz hundido el barco 3 del oponente!"<<endl;
+				if(barco14==0)
+					cout<<"Haz hundido el barco 4 del oponente!"<<endl;
 
 			}else{
 				cout<<"Haz fallado el tiro";
@@ -51,6 +101,42 @@ int main(){
 
 
 	return 0;
+}
+
+string random(){
+	string palabra="";
+	int prueba= rand() % 7 + 1;
+	switch(prueba){
+		case 1:{
+				palabra="Cheap_Tequila";
+			   }
+			   break;
+		case 2:{
+				palabra="Jell-O";
+			   }
+			   break;
+		case 3:{
+				palabra="Kamikaze";
+			   }
+			   break;
+		case 4:{
+				palabra="MindEraser";
+			   }
+			   break;
+		case 5:{
+				palabra="Bombs";
+			   }
+			   break;
+		case 6:{
+				palabra="Fireballs";
+			   }
+			   break;
+		case 7:{
+				palabra="Whiskey";
+			   }
+			   break;
+	}
+	return palabra;
 }
 
 char** llenarmatriz1(char** matriz1, int size){
